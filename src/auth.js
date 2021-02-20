859d68c8900bf3a626fe7f370d8dd969f99d286d
Caio Gomes <caiogomes34@gmail.com>
2021-02-20 21:45:15 +0000

    feat: suporte ao webhook do twitter
    Foi adicionado o suporte ao webhook
    do twitter com a validação de crc que
    é necessária para manter o endpoint
    ativo.

---
+++ src/auth.js
@@ -0,0 +1,18 @@
+const crypto = require('crypto')
+
+
+// TODO: implementar a validação de autenticação do twitter
+module.exports.twitterAuth = async (userId) => {
+    return true
+}
+
+/**
+ * Creates a HMAC SHA-256 hash created from the app TOKEN and
+ * your app Consumer Secret.
+ * @param  token  the token provided by the incoming GET request
+ * @return string
+ */
+module.exports.crcChallenge = (crc_token, consumer_secret) => {
+  let hmac = crypto.createHmac('sha256', consumer_secret).update(crc_token).digest('base64')
+  return hmac
+}
--- src/handler.js
+++ src/handler.js
@@ -2,6 +2,43 @@
 
 const AWS = require('aws-sdk'); 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const { crcChallenge } = require('./auth')
+
+module.exports.twitterHandler = async (event, context, callback) => {
+  console.log(JSON.stringify(event))
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Ok!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  }
+  
+  callback(null, response)
+}
+
+module.exports.twitterCrc = async (event, context, callback) => {
+  const crcToken = event.queryStringParameters.crc_token
+  const consumerSecret = process.env.TWITTER_CONSUMER_API_KEY_SECRET
+  const token = crcChallenge(crcToken, consumerSecret)
+  
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        response_token: 'sha256='+token
+      },
+      null,
+      2
+    ),
+  }
+  
+  callback(null, response)
+}
 
 module.exports.status = async (event, context, callback) => {
   const response = {
@@ -14,34 +51,38 @@ module.exports.status = async (event, context, callback) => {
       null,
       2
     ),
-  };
+  }
   
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+  callback(null, response)
+}
 
 module.exports.createPost = async (event, context, callback) => {
   const requestBody = JSON.parse(event.body);
   const text = requestBody.text;
   
   if (!text) {
-    return {
+    const response = {
       statusCode: 400,
       body:JSON.stringify({
         message: 'text message must not be null',
         input: event
       })
     }
+    
+    callback(response, null)
   }
+  
+  // TODO: auth the user to make a post or use recaptcha to avoid bots
+  // TODO: integrate with the tweeter api to send a tweet in the account
   
   const date = (new Date()).toISOString();
   const tableData = {
     TableName: process.env.MESSAGES_TABLE,
     Item: {
       date: date,
-      text: text
+      text: text,
+      userId: '',
+      tweetId: '',
     },
   };
   
@@ -49,25 +90,34 @@ module.exports.createPost = async (event, context, callback) => {
     await dynamoDb.put(tableData).promise();
   } catch(err) {
     console.trace(err);
-    return {
+    const response = {
       statusCode: 500,
       body:JSON.stringify({
         message: err,
         input: event
       })
     }
+    
+    callback(response, null)
   }
   
-  return {
+  const response = {
     statusCode: 200,
     body:JSON.stringify({
       message: 'message inserted',
       input: event
     })
   }
+  
+  callback(null, response)
 }
 
 module.exports.listPostsForUser = async (event, context, callback) => {
+  
+}
+
+module.exports.getPost = async (event, context, callback) => {
+  // TODO: implement the method to get a post from it's id with security analysis
   
 }
 
