736c650e4ed444f39af03edf253b689a5fab3815
Caio Gomes <caiogomes34@gmail.com>
2021-03-16 09:29:23 -0300

    Processamento de mensagens
    O processamento de mensagens para tornar
    possível cancelar ou adicionar uma confissão
    dada uma mensagem recebida pela dm do twitter.

---
+++ src/processMessages/index.js
@@ -0,0 +1,69 @@
+/**
+ * Seleciona qual ação deve ser realizada, é a função que define o processamento de texto.
+ *
+ * @param {any} payload O payload de mensagem recebido.
+ * @returns {
+ *  {
+ *    action: string,
+ *    messages: {
+ *      userId: string,
+ *      message: string}[]
+ *    }
+ *  } A ação que foi escolhida e executada pela função.
+ */
+const selectAction = (payload) => {
+  const receivedMessagesEvents = payload.direct_message_events?.filter(
+    (event) =>
+      event.type === 'message_create' &&
+      event.message_create.sender_id !== payload.for_user_id &&
+      event.message_create.message_data.text
+  )
+
+  if (!receivedMessagesEvents || receivedMessagesEvents.length === 0) {
+    return { action: 'IGNORE' }
+  }
+
+  // TODO: adicionar processamento de texto para escolher a intent do usuário.
+
+  const shouldCancelConfession = receivedMessagesEvents
+    .map((event) => isCancelMessage(event.message_create.message_data.text))
+    .reduce((prev, current) => prev || current, false)
+
+  if (shouldCancelConfession) {
+    return { action: 'CANCEL_CONFESSION' }
+  }
+
+  return {
+    action: 'CONFESS',
+    messages: receivedMessagesEvents.map((event) => ({
+      userId: event.message_create.sender_id,
+      message: event.message_create.message_data.text,
+    })),
+  }
+}
+
+/**
+ * Verifica se a mensagem é desejando cancelar confissão.
+ *
+ * @param {string} message A mensagem recebida na DM.
+ */
+const isCancelMessage = (message) => {
+  const processedMessage = message
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .trim()
+    .toLocaleUpperCase()
+
+  const commands = [
+    'CANCELAR',
+    'CANCELAR CONFISSAO',
+    'NAO POSTAR',
+    'CANCELA',
+    'NAO POSTA',
+  ]
+  return commands.includes(processedMessage)
+}
+
+module.exports = {
+  selectAction,
+}
---
+++ src/sqs/manageQueue.js
@@ -0,0 +1,9 @@
+
+/**
+ * 
+ * @param {{userId: string, timestamp: string}} messageData Informações da mensagem que o usuário enviou.
+ * @param {AWS.SQS} queue 
+ */
+const pushToQueue = (messageData, queue) => {
+
+}
